f510b4e434e3a9de5e56350db1382bc3e9f47b06
l.cs.dorina <l.cs.dorina@gmail.com>
2025-09-15 09:14:19 +0200

    baddie

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors"
+import * as path from "node:path"
+import * as url from "node:url"
 
 const PORT = 8000;
 const HOST = "127.0.0.1";
 
 //szerver inditása:
 const server = express();
+//const __filename=url.fileURLToPath(import.meta.filename)
+//console.log("Könyvtár: ",import.meta.dirname);
+const __dirname = import.meta.dirname
+const staticFilesDir = path.join(__dirname, "..", "dist")
+
 //cors problemakra -csak fehlesztesi idoben:minden kerest engelyezunk
 server.use(cors());
-server.use(express.json())
+//server.use(express.json())
+server.use(express.static(staticFilesDir))
 server.listen(PORT, () =>console.log(`server is running at http://${HOST}:${PORT}`));
